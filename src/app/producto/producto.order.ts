107abd6b21e3db195bedead4fec18e282cd65b3e
ZiberiaNn <jaramilloocamposantiago@gmail.com>
2022-06-04 11:08:38 -0500

    Thumbnails

--- src/app/producto/producto.order.ts
+++ src/app/producto/producto.order.ts
@@ -3,9 +3,11 @@ export class Producto{
     id:number;
     title:string;
     price:string;
-    constructor(id:number,title:string, price:string, date:Date, total_cost:number, is_delivered:boolean){
+    thumbnail:string;
+    constructor(id:number,title:string, price:string, date:Date, total_cost:number, is_delivered:boolean,thumbnail:string){
         this.id=id;
         this.title=title;
         this.price=price;
+        this.thumbnail=thumbnail;
     }
 }
